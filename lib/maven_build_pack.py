470620fcf12a3e8466ea7063ec3f3ee30bf2e814
Honey Bajaj <hbajaj2@sapient.com>
2016-04-10 22:05:22 +0100

    updated java download uri

--- lib/maven_build_pack.py
+++ lib/maven_build_pack.py
@@ -9,12 +9,13 @@ class CurlCustomDownloader(object):
         cmd = ["curl", "-s", "-L",
                "-o", toFile,
                "-w", "%{http_code}"]
-        if url.find('download.oracle.com') >= 0:
-            cmd.append("-H")
+        if url.find('10.113.140.187:8081') >= 0:
+            """cmd.append("-H")
             cmd.append("Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2Ftechnetwork"
                         "%2Fjava%2Fjavase%2Fdownloads%2Fjdk7-downloads-1880260.html;"
                         "oraclelicense=accept-securebackup-cookie;"
                         "s_cc=true;")
+	"""
         cmd.append(url)
         proc = Popen(cmd, stdout=PIPE)
         output, unused_err = proc.communicate()
